0ca963413e672a46075a948a6ef7bac322d8e4a9
Dung Truong <checkraiser11@gmail.com>
2020-05-07 21:36:42 +0700

    First build

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
+{-# LANGUAGE OverloadedStrings, NoImplicitPrelude #-}
 module Main where
-import ClassyPrelude
-import Lib
-
+import System.Environment (getEnv)
+import ClassyPrelude hiding (delete)
+import Web.Scotty.Trans
 main :: IO ()
-main = someFunc
+main = do
+  envPort <- getEnv "PORT"
+  let mayPort = readMay envPort
+  case mayPort of
+    Just port -> scottyT port id routes
+    Nothing -> putStrLn "Error"
+  
+routes :: (MonadIO m) => ScottyT LText m ()
+routes =
+  get "/hello" $ text "Hello!"
