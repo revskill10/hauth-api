265756cbc0d02474bd5439c4a181bddd2695ba4c
Dung Truong <checkraiser11@gmail.com>
2020-05-07 22:10:11 +0700

    Add cors

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,16 @@ module Main where
 import System.Environment (getEnv)
 import ClassyPrelude hiding (delete)
 import Web.Scotty.Trans
+import Network.Wai.Middleware.Cors
 main :: IO ()
 main = do
+  
   envPort <- getEnv "PORT"
   let mayPort = readMay envPort
   case mayPort of
-    Just port -> scottyT port id routes
+    Just port -> scottyT port id $ do
+      middleware simpleCors
+      routes
     Nothing -> putStrLn "Error"
   
 routes :: (MonadIO m) => ScottyT LText m ()
