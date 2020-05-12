c746a2c531bfcad531163e79a507cf8f49e4805c
Dung Truong <checkraiser11@gmail.com>
2020-05-12 21:06:01 +0700

    Fix

--- app/Main.hs
+++ app/Main.hs
@@ -6,21 +6,23 @@ import Web.Scotty.Trans
 import Network.Wai.Middleware.Cors
 import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.URL
+import qualified Data.Text as T
 defaultPort = 3000
 main :: IO ()
 main = do
   envPort <- getEnv "PORT"
   connectInfoEnv <- getEnv "DATABASE_URL"
-  let connectInfo = parseDatabaseUrl =<< readMay connectInfoEnv
+  let connectInfo = parseDatabaseUrl connectInfoEnv
   let mayPort =  readMay envPort
   case mayPort of
     Just port -> scottyT port id $ do
       middleware simpleCors
       case connectInfo of
-        Nothing -> notFound (text "Error")
+        Nothing -> notFound (text "Error leu leu")
         Just conn -> routes conn
     Nothing -> error "Error"
   
 routes :: (MonadIO m) => ConnectInfo -> ScottyT LText m ()
 routes conn =
-  get "/hello" $ text (pack $ connectHost conn)
+  get "/hello" $ text (pack $ show conn)
+
