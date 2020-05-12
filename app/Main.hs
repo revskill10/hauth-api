dc21d8cb0db7c4da093f4b3e8df83bccc5c182dd
Dung Truong <checkraiser11@gmail.com>
2020-05-12 08:46:18 +0700

    Add connect info

--- app/Main.hs
+++ app/Main.hs
@@ -4,17 +4,24 @@ import System.Environment (getEnv)
 import ClassyPrelude hiding (delete)
 import Web.Scotty.Trans
 import Network.Wai.Middleware.Cors
+import Database.PostgreSQL.Simple.URL
+
+defaultConn = "test"
+
 main :: IO ()
 main = do
-  
   envPort <- getEnv "PORT"
+  connectInfoEnv <- getEnv "DATABASE_URL"
+  let connectInfo = parseDatabaseUrl <$> connectInfoEnv
   let mayPort = readMay envPort
   case mayPort of
     Just port -> scottyT port id $ do
       middleware simpleCors
-      routes
+      case connectInfo of
+        Nothing -> routes defaultConn
+        Just conn -> routes conn
     Nothing -> putStrLn "Error"
   
-routes :: (MonadIO m) => ScottyT LText m ()
-routes =
-  get "/hello" $ text "Hello!"
+routes :: (MonadIO m) => ConnectInfo -> ScottyT LText m ()
+routes conn =
+  get "/hello" $ text ("Hello!" ++ connectHost conn)
