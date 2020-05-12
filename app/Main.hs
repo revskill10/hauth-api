ea90a9d9c6b15a12a86c740af7688edd0e6c48cc
Dung Truong <checkraiser11@gmail.com>
2020-05-12 19:32:56 +0700

    Add connection string

--- app/Main.hs
+++ app/Main.hs
@@ -6,23 +6,21 @@ import Web.Scotty.Trans
 import Network.Wai.Middleware.Cors
 import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.URL
-
-defaultConn = "test"
 defaultPort = 3000
 main :: IO ()
 main = do
   envPort <- getEnv "PORT"
   connectInfoEnv <- getEnv "DATABASE_URL"
-  let connectInfo = parseDatabaseUrl <$> connectInfoEnv
-  let mayPort = readMay envPort
+  let connectInfo = parseDatabaseUrl =<< readMay connectInfoEnv
+  let mayPort =  readMay envPort
   case mayPort of
     Just port -> scottyT port id $ do
       middleware simpleCors
       case connectInfo of
-        Nothing -> routes defaultConn
+        Nothing -> notFound (text "Error")
         Just conn -> routes conn
-    Nothing -> putStrLn "Error"
+    Nothing -> error "Error"
   
 routes :: (MonadIO m) => ConnectInfo -> ScottyT LText m ()
 routes conn =
-  get "/hello" $ text ("Hello!" ++ connectHost conn)
+  get "/hello" $ text (pack $ connectHost conn)
